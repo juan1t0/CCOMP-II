17f3a2c9851809f03ad58a71adfdf9cb46472053
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2017-03-28 07:50:20 -0500

    max

--- main.cpp
+++ main.cpp
@@ -5,24 +5,27 @@
 using namespace std;
 
 string dig1[10]={"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
-string dig2[5]={"once","doce","trece","catorce","quince"};
+string dig2[10]={"once","doce","trece","catorce","quince","dieciseis","diecisiete","dieciocho","diecinueve"};
 string dig3[10]={"","diez","veinte","treinta","cuarenta","cincuenta","sesenta","setenta","ochenta","noventa"};
 string dig4[4]={"cien","ciento","cientos","entos"};
 string dig5[8]={"do","tre","cuatro","quini","seis","sete","ocho","nove"};
 string dig6[3]={"mil","millon","millones"};
 
-string escribe_0to99(int numero){
+string escribe_0to99(long int numero){
 
     if (numero < 10)
         return dig1[numero];
     else if(numero%10 == 0)
         return dig3[(numero / 10)];
-    else if(numero<=15)
-        return dig2[(numero -11)];
-    else if(numero >15 && numero < 100)
+    else if(numero<20&&numero>10)
+        return dig2[(numero %10)-1];
+    else if(numero<30&&numero>20)
+        return "veinti"+dig1[numero % 10];
+    else if(numero >30 && numero < 100){
         return dig3[(numero / 10)] +" y "+dig1[(numero % 10)];
+    }
 }
-string escribe_100to999(int numero){
+string escribe_100to999(long int numero){
     if(numero ==0)
         return "";
     if (numero <100)
@@ -44,7 +47,7 @@ string escribe_100to999(int numero){
         return dig5[(numero / 100)-2]+ dig4[2] +" "+( escribe_0to99(numero % 100));
 }
 
-string escribe_mil(int numero){
+string escribe_mil(long int numero){
     if(numero==0)
         return "";
     if(numero<1000)
@@ -55,15 +58,17 @@ string escribe_mil(int numero){
         return escribe_100to999(numero /1000)+" "+dig6[0]+" "+escribe_100to999(numero %1000);
 
 }
-string escribe_millones(int numero){
-    //if(numero < 100000000)
-      //  return escribe_0to99(numero / 1000000)+" "+dig6[2]+" "+escribe_mil(numero %1000000);
+string escribe_millones( long int numero){
+    if(numero < 1000000)
+        return escribe_mil(numero);
+    if(numero < 100000000)
+        return escribe_0to99(numero / 1000000)+" "+dig6[2]+" "+escribe_mil(numero %1000000);
     if(numero < 1000000000)
         return escribe_100to999(numero/10000000)+" "+dig6[2]+" "+escribe_mil(numero %1000000);
     else if(numero < 1000000000000)//999000-999000
         return escribe_mil(numero/1000000)+" "+dig6[2]+" "+escribe_mil(numero%1000000);
-}
-string escribe( int numero){
+}/*
+string escribe( long int numero){
     if (numero<100)
         return escribe_0to99(numero);
     else if(numero < 1000)
@@ -74,12 +79,12 @@ string escribe( int numero){
         return escribe_0to99(numero / 1000000)+" "+dig6[2]+" "+escribe_100to999(numero %1000)+" "+dig6[0]+" "+escribe_100to999(numero %1000);
     else if(numero < 10000000000)//999 19999999
         return escribe_100to999(numero/10000000)+" "+dig6[2]+" "+escribe_100to999(numero %1000)+" "+dig6[0]+" "+escribe_100to999(numero %1000);
-}
+}*/
 int main()
 {
     cout<<"ingrese un numero : "<<endl;
-    int num;
-  //  cin>>num;
-    cout<<escribe_mil(21000)<<endl;;
+    long int num;
+    cin>>num;
+    cout<<escribe_millones(num)<<endl;;
     return 0;
 }
