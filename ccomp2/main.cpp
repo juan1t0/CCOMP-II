93b8a2fd3f21d41d781a7ea7156bd1ea3e65f421
juan1t0 <juanpablo.heredia@ucsp.edu.pe>
2017-04-18 07:57:41 -0500

    recurcitos

---
+++ ccomp2/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+void swap(int *a , int *b){
+	int *aux;
+	*aux=*a;
+	*a=*b;
+	*b=*aux;
+}
+int* crearL(int len){
+	srand(time(NULL));
+	int *a,*g;
+	g=a;
+	for(int i=0;i<len;i++,a++)
+		*a=rand()%50;
+	return g;
+}
+void imprimir(int *w,int len){
+	for(int i=0;i<len;i++,w++)
+		cout<<*w<<",";
+}
+int main (int argc, char *argv[]) {
+	int* ar = crearL(10);
+	imprimir(ar,10);
+	return 0;
+}
+
